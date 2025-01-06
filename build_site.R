9724f3e3ba7a32c4ffe70e2d97400f15fa212ca3
Kylie A. Bemis <k.bemis@northeastern.edu>
2025-01-06 11:06:23 -0500

    fix R4DS 2e link

--- build_site.R
+++ build_site.R
@@ -2,7 +2,7 @@
 # This helps avoid confusion if our working directory is 
 # not our site because of other projects we were 
 # working on at the time. 
-setwd("~/Documents/Developer/Website/kuwisdelu.github.io")
+setwd("~/Dropbox/Website/kuwisdelu.github.io")
 
 # Render your site. 
 rmarkdown::render_site()
