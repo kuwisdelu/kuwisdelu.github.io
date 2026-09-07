515242d1e9b97de502bb9684a0b166f0f7480bbb
Kylie Bemis <k.bemis@northeastern.edu>
2026-09-07 18:14:39 -0400

    add fall 2026 course pages

--- build_site.R
+++ build_site.R
@@ -2,7 +2,7 @@
 # This helps avoid confusion if our working directory is 
 # not our site because of other projects we were 
 # working on at the time. 
-setwd("~/Dropbox/Website/kuwisdelu.github.io")
+setwd("~/Dropbox/Developer/kuwisdelu.github.io")
 
 # Render your site. 
 rmarkdown::render_site()
